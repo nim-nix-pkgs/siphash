e169e57e6a5a68a1b30082178977d6c726509357
hugosenari <hugosenari@gmail.com>
2022-03-17 18:28:37 -0300

    chore: re-index ../nimpkgs/s/siphash/master

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-siphash-master.owner = "~ehmry";
   inputs.src-siphash-master.ref   = "refs/heads/master";
   inputs.src-siphash-master.repo  = "nim_siphash";
-  inputs.src-siphash-master.type  = "other";
+  inputs.src-siphash-master.type  = "sourcehut";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
